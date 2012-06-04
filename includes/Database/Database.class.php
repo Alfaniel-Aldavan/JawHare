852d82d5650ec6ea5040fd02f17895c29fd6e1e1
Michael Miller <mikemill@gmail.com>
2012-06-03 21:34:56 -0700

    Initial push to write phpDocs.  Incomplete.
    Signed-off-by: Michael Miller <mikemill@gmail.com>

--- includes/Database/Database.class.php
+++ includes/Database/Database.class.php
@@ -1,13 +1,42 @@
 <?php
 
 namespace JawHare\Database;
+
+/**
+ * The abstract base database class. 
+ */
 abstract class Database
 {
+	/**
+	 * Stores the connection resources in an associative array
+	 * @var array
+	 */
 	protected $conns = array();
+	
+	/**
+	 * The settings passed to this database object
+	 * @var array
+	 */
 	protected $settings = array();
+	
+	/**
+	 * The name of the implemented database class.
+	 * Used to load database specific storage classes
+	 * @var string
+	 */
 	protected $dbtypename = '';
+	
+	/**
+	 * Stores the various database storages that have been created.
+	 * Done so the storages can be reused.
+	 * @var array
+	 */
 	protected $dbstorages = array();
 
+	/**
+	 *
+	 * @param array $settings The database configurations
+	 */
 	public function __construct($settings)
 	{
 		$this->settings = $settings;
@@ -15,6 +44,14 @@ abstract class Database
 		$this->connect();
 	}
 
+	/**
+	 * Executes the specified query after doing any replacements
+	 * @param string $query The query to execute
+	 * @param array $replacements The replacement variables
+	 * @param string $conn Defaults to 'host'.  The connection to use for this query.
+	 * @return \JawHare\Database\DatabaseResult
+	 * @throws DatabaseException 
+	 */
 	abstract public function query($query, $replacements = array(), $conn = 'host');
 	abstract public function insert($table, $columns, $data, $querytype = 'insert', $conn = 'write');
 	abstract public function update($table, $columns, $data, $conn = 'write');
